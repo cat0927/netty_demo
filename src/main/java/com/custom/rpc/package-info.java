ef68a1986512b95a3adab973486393bf2c5a4a78
cat0927 <17037159+cat0927@users.noreply.github.com>
2020-05-08 00:56:32 +0800

    update

--- src/main/java/com/custom/rpc/package-info.java
+++ src/main/java/com/custom/rpc/package-info.java
@@ -10,6 +10,13 @@ package com.custom.rpc;
  *
  * RpcConsumer (作为Client )
  * 调用 Service -》 Service 利用反射原理，通过类名、方法名.调用方法
+ *
+ *
+ * 注意：
+ * unsupported message type: InvokerProtocol (expected: ByteBuf, FileRegion)
+ *
+ *  自定义协议，要实现序列化
+ *  InvokerProtocol implements Serializable
  */
 
 
